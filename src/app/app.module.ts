1f4203df94be2122df98d8f1a0fcb4f917d8f3a5
saulmartinez.llevele <saulmartinez.llevele@gmail.com>
2022-10-04 17:51:32 -0500

    Dark and light mode configurations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ProvidersModule } from './providers/providers.module';
+
 
 @NgModule({
   declarations: [
@@ -10,9 +13,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [
+    ProvidersModule
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
